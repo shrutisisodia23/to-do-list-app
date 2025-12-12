d130d9b36127f64be20651088cb26d6a5079ab86
shrutisisodia23 <shrutisisodia23@gmail.com>
2025-12-12 23:43:16 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['bootstrap/dist/css/bootstrap.min.css']
+      external: [
+        'bootstrap/dist/css/bootstrap.min.css',
+        'bootstrap/dist/js/bootstrap.bundle.min.js'
+      ],
+      output: {
+      }
     }
+  },
+  optimizeDeps: {
+    include: ['bootstrap']
   }
-})
+});
