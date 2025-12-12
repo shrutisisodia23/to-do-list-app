70dec6b33ea344b7071882975de78730de5ddf74
shrutisisodia23 <shrutisisodia23@gmail.com>
2025-12-12 23:07:20 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
